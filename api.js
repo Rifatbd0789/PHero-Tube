81471f00c97bc6aa7b8730e34897e73649883539
Rifat Rabbi <rifayrabby78@gmail.com>
2023-08-31 19:41:50 +0600

    category part done

--- api.js
+++ api.js
@@ -1,1 +1,17 @@
+const category = async () => {
+  const res = await fetch(
+    "https://openapi.programming-hero.com/api/videos/categories"
+  );
+  const data = await res.json();
+  const categoryField = document.getElementById("categoryField");
+  data.data.forEach((element) => {
+    const newButton = document.createElement("div");
+    newButton.innerHTML = `
+    <button class="btn bg-lal text-white">${element.category}</button>
+    `;
+    categoryField.appendChild(newButton);
+    console.log(element.category);
+  });
+};
+category();
 // console.log('JS is connected')
